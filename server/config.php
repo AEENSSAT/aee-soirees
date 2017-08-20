b71a95aef42b0800742cced85d57d29f54e077c6
= <luuka.tr@gmail.com>
2017-08-20 12:25:02 +0200

    rm pwd

--- server/config.php
+++ server/config.php
@@ -1,6 +1,6 @@
 <?php
     define("CONFIG_USER_LOGIN","admin");
-    define("CONFIG_USER_PASSWORD", "argentIMR61");
+    define("CONFIG_USER_PASSWORD", "admin");
 
     define("CONFIG_DATABASE_HOST","localhost");
     define("CONFIG_DATABASE_USER","bourse");
